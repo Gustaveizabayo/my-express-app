f7a05b1fc6c1d539ea3e4a865af10e02bbe19c08
izabayo gustave <izabayogustave54@gmail.com>
2026-03-27 14:57:54 +0200

    adding cruud

--- server.js
+++ server.js
@@ -16,6 +16,8 @@ app.get('/', (req, res) => {
     });
 });
 
+app.use('/api/fruits', require('./routes/routes'));
+
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
